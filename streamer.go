f6b5c6e7f34485bf49b2a1466566b956c17315f3
kazukgw <kazukgw@gmail.com>
2018-02-04 03:22:26 +0000

    Update streamer

--- streamer.go
+++ streamer.go
@@ -1,6 +1,8 @@
 package mydump2bq
 
 import (
+	"context"
+
 	"cloud.google.com/go/bigquery"
 )
 
@@ -9,9 +11,37 @@ type BigQueryStreamer struct {
 	TableID   string
 	*MySQLTable
 	*bigquery.Uploader
+	InCh     chan *Row
+	StopCh   chan struct{}
+	FinishCh chan struct{}
 }
 
 func NewBigQueryStreamer(datasetID, tableID string) *BigQueryStreamer {
 	// u := client.Dataset(datasetID).Table(tableID).Uploader()
 	return nil
+}
+
+func (st *BigQueryStreamer) Start() {
+	bufsize := 100
+	buf := make([]*Row, bufsize)
+	seqnum := 0
+	go func() {
+		for {
+			select {
+			case row := <-st.InCh:
+				buf = append(buf, row)
+				seqnum += 1
+				if len(buf) == bufsize {
+					go func(rows []*Row) {
+						ctx := context.Background()
+						st.Uploader.Put(ctx, rows)
+					}(buf)
+					buf = make([]*Row, bufsize)
+				}
+			case <-st.StopCh:
+				break
+			}
+		}
+		st.FinishCh <- struct{}{}
+	}()
 }
